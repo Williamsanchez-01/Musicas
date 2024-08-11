90788ef4835e1d610713fe4907fbb8904d420c39
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-08-11 14:09:09 -0300

    Finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.Write("Como Tudo Deve Ser");
+Console.WriteLine("\n");
+
+Console.ResetColor();
+
+Console.Write(@"Um belo sonho veio então despertar minha vontade
+Tudo vale a pena pra te encontrar
+Me livrei de tudo aquilo e consegui mudar
+Tudo que foi feito em troca de uma amizade
+Mas felicidade é poder estar com quem
+Você gosta em algum lugar
+É foda ser louco advogado do mundo");
+
+Console.WriteLine("\n");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine("Mas, como tudo deve ser");
+
+Console.ResetColor();
+
+Console.WriteLine("");
+
+Console.Write("É foda ser tachado de doido Vagabundo");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Mas,como tudo deve ser");
+
+Console.ResetColor();
+
+Console.WriteLine("");
+
+Console.Write(@"Foi quando te encontrei
+Ouvindo o som e olhando o mar
+Foi quando te encontrei
+Ouvindo o som do mar rolar
+Eu não nasci ontem, bem quando e como por onde mais");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Como tudo deve ser");
+
+Console.ResetColor();
+
+Console.WriteLine("");
+
+Console.Write(@"Com as balizas do nosso sistema
+Sigo imprimindo meu sonho na história");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Como tudo deve ser");
+
+Console.ResetColor();
+
+Console.WriteLine("");
+
+Console.Write(@"Um belo sonho veio então despertar minha vontade
+Tudo vale a pena pra te reencontrar
+Me livrei de tudo aquilo e consegui mudar
+Tudo que foi feito em troca de uma amizade mas");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine("\n");
+
+Console.WriteLine(@"Foi quando te encontrei
+Ouvindo o som e olhando o mar
+Foi quando te encontrei
+Ouvindo o som do mar rolar");
+
+Console.ResetColor();
+
+Console.WriteLine("");
+
+Console.Write(@"Eu não preciso de promessas
+E acho que você também,
+Eu não tento ser perfeito
+E acho que você também
+Dias e noites, pensando no que fiz
+Eu sou um vencedor
+Eu lutei pelo o que eu quis
+Mas quando não se pode mais mudar tanta coisa errada
+Vamos viver os nossos sonhos
+Temos tão pouco tempo");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine("\n");
+
+Console.Write(@"Foi quando te encontrei
+Ouvindo o som e olhando o mar
+Foi quando te encontrei
+Ouvindo o som do mar rolar
+Foi quando te encontrei,
+Ouvindo o som e olhando o mar
+Foi quando te encontrei
+Ouvindo o som do mar rolar
+Foi quando te encontrei,
+Ouvindo o som e olhando o mar
+Ouvindo o som o mar rolar
+O mar");
+
+Console.ResetColor();
